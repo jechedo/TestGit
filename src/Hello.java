d60f3a33a7bb6bea2dda6be9e0032876c2346e2e
jechedo <jechedo@163.com>
2017-09-26 20:18:20 +0800

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Description:
+ *
+ * @author LiXiaoCong
+ * @version 2017/9/26 20:13
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
